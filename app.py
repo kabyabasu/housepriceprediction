858fde0e9adac101d1f73d1ae0a1de7f934e58b1
kabyabasu <kabyabasu@yahoo.co.uk>
2022-11-20 21:50:10 +0530

    commiting scaler.pkl and app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+import json
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+regModel = pickle.load("regression.pkl","rb")
+scaler = pickle.load("scaler.pkl","rb")
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',method=['post'])
+def predict_api():
+    data = request.json['data']
+    data_trasform= scaler.tranform(np.array(list(data.values())).reshape(1,-1))
+    output = regModel.predict(data_trasform)
+    return jsonify(output[0])
+
+
+
+
+
+
